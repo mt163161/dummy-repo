89baba116fe89313c1d50955b3b51736bb6ba39f
mt163161 <mxterania@gmail.com>
2022-07-28 23:31:38 +0800

    test library

--- jenkins/jenkinsfiles/jenkinsfile-sample.groovy
+++ jenkins/jenkinsfiles/jenkinsfile-sample.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-// @Library('pipeline-library-demo@main')_
+@Library('pipeline-library-demo@main')_
 
 node {
     stage('Greetings') {
         echo "Good Day!"
-        // sayHello 'Dave'
+        sayHello 'Dave'
     }
 }
